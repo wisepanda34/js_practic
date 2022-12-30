bed9111b1e5c8a42b3b63c235500aab892614603
Yevgen Naidenko <evghen.naydenko@gmail.com>
2022-12-30 22:11:37 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,15 @@
+"use strict";
+
+// const answers = [];
+/* answers[0] = prompt("What is your name?", '');
+answers[1] = prompt("What is your surname?", '');
+answers[2] = prompt("How is your old?", '');
+
+document.write(answers); */
+// console.log(typeof (answers));  
+
+const category = 'toys';
+console.log(`https://someurl.com/${category}/5`);
+
+const user = "Ivan";
+alert(`Hello, ${user}`);
